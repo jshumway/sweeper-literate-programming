b9b387e5dbe4c87195d867d38400059786e3aca1
Jared Shumway <jaredkshumway@gmail.com>
2020-08-20 22:56:26 -0600

    Sweeper: flowers -> sweeper

--- sweeper/main.lua
+++ sweeper/main.lua
@@ -3,4 +3,4 @@ fennel = require("lib.fennel")
 table.insert(package.loaders, fennel.make_searcher({correlate=true}))
 pp = function(x) print(require("lib.fennelview")(x)) end
 
-require("flowers")
+require("sweeper")
